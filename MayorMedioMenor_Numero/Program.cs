c91099bb0c0597dfe58f472fc868baebba5c0ca8
Eisner Lopez Acevedo <elopeza@ucatolica.ac.cr>
2016-06-20 11:40:37 -0600

    Inicio del programa

--- MayorMedioMenor_Numero/Program.cs
+++ MayorMedioMenor_Numero/Program.cs
@@ -10,6 +10,61 @@ namespace MayorMedioMenor_Numero
     {
         static void Main(string[] args)
         {
+                int num, a, b, c;
+                Console.WriteLine("Ingrese primer numero: ");
+                a = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese segundo numero: ");
+                b = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese tercer numero: ");
+                c = Int32.Parse(Console.ReadLine());
+
+                if ((a > b) && (a > c))
+                {
+                    Console.WriteLine("{0} es el mayor ", a);
+                if (b > c)
+                {
+                    Console.WriteLine("{0} es el medio ", b);
+                    Console.WriteLine("{0} es el menor ", c);
+                }
+                else
+                {
+                    if (c > b)
+                    {
+                        Console.WriteLine("{0} es el medio ", c);
+                        Console.WriteLine("{0} es el menor ", b);
+                    }
+                }
+                }
+                else
+                {
+                    if ((b > a) && (b > c))
+                    {
+                        Console.WriteLine("{0} es el mayor ", b);
+                    if (a > c)
+                    {
+                        Console.WriteLine("{0} es el medio ", a);
+                        Console.WriteLine("{0} es el menor ", c);
+                    }
+                    else
+                    {
+                        if (c > a)
+                        {
+                            Console.WriteLine("{0} es el medio ", c);
+                            Console.WriteLine("{0} es el menor ", a);
+                        }
+                    }
+                }
+
+                    else
+                    {
+                        if ((c > a) && (b > a))
+                        {
+                            Console.WriteLine("{0} es el mayor ", c);
+                        }
+                    }
+                }
+                Console.ReadKey();
+
+            }
         }
     }
-}
